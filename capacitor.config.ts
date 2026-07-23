167fff5f160985cfda44285bfe26beb9803bb0db
Shovo <shovo@Shovos-MacBook-Air.local>
2026-07-23 09:50:09 +0600

    add capacitor config & android platform

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.smtfamily.app',
+  appName: 'SMT Family',
+  webDir: 'dist'
+};
+
+export default config;
